04adcb84559653e03e9eff599d31bd455db7047d
Jason <jason.afonso.fernandes@gmail.com>
2024-04-25 01:33:20 +0200

    Menu Karaoke + debut invitation joueurs

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
+
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
+    WA.room.hideLayer("rap-room")
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA.player.tags)
+    
+    
+    
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
@@ -18,6 +24,46 @@ WA.onInit().then(() => {
     })
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
+
+
+    const subscription = WA.event.on("INVITATION_KARAOKE").subscribe( async (event) => {
+        const myWebsite = await WA.ui.website.open({
+            url: "./src/invit.html",
+            position: {
+                vertical: "top",
+                horizontal: "left",
+            },
+            size: {
+                height: "20vh",
+                width: "20vw",
+            },
+        });
+        
+        myWebsite.position.vertical = "top";
+    });
+
+    // WA.room.area.onEnter('CreateGameZone').subscribe(async () => {
+    //     console.log("salut");
+    //     const myWebsite = await WA.ui.website.open({
+    //         url: "./src/index.html",
+    //         position: {
+    //             vertical: "middle",
+    //             horizontal: "middle",
+    //         },
+    //         size: {
+    //             height: "50vh",
+    //             width: "50vw",
+    //         },
+    //     });
+
+    //     myWebsite.position.vertical = "top";
+    //     WA.room.area.onLeave("CreateGameZone").subscribe(() => {
+    //         myWebsite.close();
+    //     });
+    //   });
+
+
+
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
