47256b8f6a24153fae21eec74df20c3a85c7f1be
LucasGitB <lucasaguesse@gmail.com>
2024-04-26 14:52:49 +0200

    insert en bdd le joueur qui chante

--- src/main.ts
+++ src/main.ts
@@ -2,59 +2,82 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any = undefined;
 
-
-
 // Waiting for the API to be ready
-WA.onInit().then(async () => {
+WA.onInit()
+  .then(async () => {
     //WA.room.hideLayer("rap-room")
-    console.log('Scripting API readyiiiii');
-
+    console.log("Scripting API readyiiiii");
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API Extra ready");
+      })
+      .catch((e) => console.error(e));
 
-    const myAreaSubscriber = WA.room.area.onEnter("singerZone").subscribe(() => {
-        const playerId = WA.player.uuid
-        console.log(playerId)
-        
+    const myAreaSubscriber = WA.room.area
+      .onEnter("singerZone")
+      .subscribe(() => {
+        const playerId = WA.player.uuid;
+        console.log(playerId);
+
+        // Définissez les données du joueur à envoyer
+        const playerData = {
+          playerId: playerId,
+          // Ajoutez d'autres champs si nécessaire, comme la date de naissance, etc.
+        };
+
+        // Envoie de la requête POST à l'API
+        fetch("http://localhost:3000/players", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(playerData),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log("Nouveau joueur inséré:", data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
       });
-    
-    
-    
-    const subscription = WA.event.on("New-Game").subscribe((event) => {
-        console.log("Event received", event.data);
-        const x = event.data.pos.x
-        const y = event.data.pos.y
-        console.log("ptit test"+event.data.invitFor.indexOf(WA.player.name))
-        if(event.data.invitFor.indexOf(WA.player.name)!=-1){
-            WA.ui.actionBar.addButton({
-                id: 'register-btn',
-                type: 'action',
-                imageSrc: '../public/images/favicon.svg',
-                toolTip: 'Rejoindre',
-                callback: (event) => {
-                    console.log('Button clicked', event);
-                    // When a user clicks on the action bar button 'Register', we remove it.
-                    
-                    WA.player.teleport(x, y);
-                    WA.ui.actionBar.removeButton('register-btn');
-                }
-            });
-        }else{
-            console.log("non")
-        }
-        
+
+    const subscription = WA.event.on("New-Game").subscribe((event: any) => {
+      console.log("Event received", event.data);
+      const x: number = event.data.pos.x;
+      const y: number = event.data.pos.y;
+      console.log("ptit test" + event.data.invitFor.indexOf(WA.player.name));
+      if (event.data.invitFor.indexOf(WA.player.name) !== -1) {
+        WA.ui.actionBar.addButton({
+          id: "register-btn",
+          type: "action",
+          imageSrc: "../public/images/favicon.svg",
+          toolTip: "Rejoindre",
+          callback: (event: any) => {
+            console.log("Button clicked", event);
+            // When a user clicks on the action bar button 'Register', we remove it.
+            WA.player.teleport(x, y);
+            WA.ui.actionBar.removeButton("register-btn");
+          },
+        });
+      } else {
+        console.log("non");
+      }
     });
 
-    WA.state.onVariableChange('gamesState').subscribe((value) => {
-        console.log("salutttttttt"+value);
-        
+    WA.state.onVariableChange("gamesState").subscribe((value) => {
+      console.log("salutttttttt" + value);
     });
 
     // WA.room.area.onEnter('CreateGameZone').subscribe(async () => {
@@ -76,19 +99,14 @@ WA.onInit().then(async () => {
     //         myWebsite.close();
     //     });
     //   });
+  })
+  .catch((e) => console.error(e));
 
-
-
-
-    
-
-}).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function closePopup() {
+  if (currentPopup !== undefined) {
+    currentPopup.close();
+    currentPopup = undefined;
+  }
 }
 
 export {};
